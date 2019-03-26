246029b7efcc778152fe58d61132dcba6ced9510
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-26 13:26:51 -0400

    Updates the roles-router.js file with commented out stepsto follow for implementing knex statements to manipulate data in the database

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const knex = require('knex'); // Step 1: Bring in the library
 
-const knexConfig = { // Step 2: Configure the library being used
+const knexConfig = { // Step 2: Configure the knexjs library being used
   client: 'sqlite3', // this is the driver
   useNullAsDefault: true,
   connection: {
@@ -9,9 +9,10 @@ const knexConfig = { // Step 2: Configure the library being used
   },
 };
 
-const db = knex(knexConfig); // we are calling the database 'db'
+const db = knex(knexConfig); // Step 3: Set the 'knex(knexConfig)' to a variable called 'db'
 
 router.get('/', (req, res) => {
+  // Step 4: Use the proper syntax from knexjs.org to select and return all records
   // returns a promise that resolves to all records in the table
   db('roles').then(roles => {
     res.status(200).json(roles);
