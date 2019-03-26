eb9a95d91c1a8e37e6f7d7efe828e11d024f4d2e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-26 13:46:42 -0400

    Creates a db() query statement inside of 2nd request handler to return roles by the specified id in the request

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -23,8 +23,17 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
   // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+  db('roles')
+    .where({ id })
+    .first() // this makes sure that we're only grabbing the FIRST element that matches our conditions
+    .then(role => {
+      res.status(200).json(role);
+    }).catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/', (req, res) => {
