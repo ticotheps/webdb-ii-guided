807e06843a57c8422f31334c4103618fdfb108d4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-26 13:16:52 -0400

    Creates a db statement inside of the first request handler in roles-router.js to return all records from the database using knex

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,17 +1,24 @@
 const router = require('express').Router();
-const knex = require('knex');
+const knex = require('knex'); // Step 1: Bring in the library
 
-const knexConfig = {
-  client: 'sqlite3',
+const knexConfig = { // Step 2: Configure the library being used
+  client: 'sqlite3', // this is the driver
   useNullAsDefault: true,
   connection: {
-    filename: '../data/roles.db3'
+    filename: '../data/roles.db3' // this is the relative path to the db file
   },
 };
 
-const db = knex(knexConfig);
+const db = knex(knexConfig); // we are calling the database 'db'
 
 router.get('/', (req, res) => {
+  // returns a promise that resolves to all records in the table
+  db('roles').then(roles => {
+    res.status(200).json(roles);
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  });
   // get the roles from the database
   res.send('Write code to retrieve all roles');
 });
